fa3b30593123a2482e4ddfe7257f3bb8d718febe
Sozonov_Ilushka <113029719+sozozzya@users.noreply.github.com>
2022-10-18 17:51:09 +0300

    Update Practice0.c

--- СозоновИС/Practice0/Practice0/Practice0.c
+++ СозоновИС/Practice0/Practice0/Practice0.c
@@ -6,21 +6,21 @@ int main()
 	setlocale(LC_ALL, "Rus");
 	setlocale(LC_NUMERIC, "C");
 	float x1, y1, r1, x2, y2, r2, d;
-	printf("Введите координаты центра первой окружности и её радиус: ");
+	printf("Enter the coordinates of the center of the first circle and its radius: ");
 	scanf("%f %f %f", &x1, &y1, &r1);
-	printf("Введите координаты центра второй окружности и её радиус: ");
+	printf("Enter the coordinates of the center of the second circle and its radius: ");
 	scanf("%f %f %f", &x2, &y2, &r2);
 	d = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 	if (d==0)
 	{
 		if (r1 == r2)
 		{
-			printf("Окружности имеют бесконечное множество общих точек");
+			printf("Circles have infinite set of common points");
 			return 0;
 		}
 		else
 		{
-			printf("Окружности не имеют ни одной общей точки");
+			printf("Circles have no common point");
 			return 0;
 		}
 	}
@@ -28,17 +28,17 @@ int main()
 	{
 		if ((d == abs(r2 - r1)) || (d == r2 + r1))
 		{
-			printf("Окружности имеют ровно одну общую точку");
+			printf("Circles have exactly one common point");
 			return 0;
 		}
 		if ((d > abs(r2 - r1)) && (d < r2 + r1))
 		{
-			printf("Окружности имеют ровно две общие точки");
+			printf("Circles have exactly two common points");
 			return 0;
 		}
 		else
 		{
-			printf("Окружности не имеют ни одной общей точки");
+			printf("Circles have no common point");
 			return 0;
 		}
 	}
